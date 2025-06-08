3d770999a43be90e332d94351f19b64897dd5d9c
Satmeet Singh <satmeetarora@gmail.com>
2025-06-08 15:00:23 +0530

    feat: modify public and nasic css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -147,7 +147,7 @@ const About = () => {
               className={styles.teamMember}
             >
               <div className={styles.teamMemberImage}>
-                <img src="./assets/Nishantkumar.jpg" alt="Nishant Kumar" />
+                <img src="./Nishantkumar.jpg" alt="Nishant Kumar" />
               </div>
               <div>
                 <h3 className={styles.teamMemberName}>Nishant Kumar</h3>
@@ -225,7 +225,7 @@ const About = () => {
               className={styles.teamMember}
             >
               <div className={styles.teamMemberImage}>
-                <img src="./assets/Satmeetsingh.jpg" alt="Satmeet singh" />
+                <img src="./Satmeetsingh.jpg" alt="Satmeet singh" />
               </div>
               <div>
                 <h3 className={styles.teamMemberName}>Satmeet singh</h3>
